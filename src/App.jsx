4d9816a2211d802b1ef97c0e5736488b66c7d2ac
sakibMERN <sakib.sr57@gmail.com>
2024-10-28 18:37:44 +0600

    project initial setup done with adding tailwind and DaisyUI

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-7xl">This is tailwind</h1>
+      <div className="badge">default</div>
+      <div className="badge badge-neutral">neutral</div>
+      <div className="badge badge-primary">primary</div>
+      <div className="badge badge-secondary">secondary</div>
+      <div className="badge badge-accent">accent</div>
+      <div className="badge badge-ghost">ghost</div>
+    </>
+  );
+}
+
+export default App;
